5920baa3a3387bc1ba2b1073310ebb292a06e635
Jordan Owusu-Ansah <jordanowusu03@gmail.com>
2026-03-03 17:48:07 +0000

    commented out voucher box

---
+++ check_order_fetch.cjs
@@ -0,0 +1,4 @@
+﻿const fs = require("fs");
+const src = fs.readFileSync("app/washstation/payment/page.tsx", "utf8");
+const idx = src.indexOf("useStationOrder");
+console.log(src.substring(idx - 50, idx + 200));
