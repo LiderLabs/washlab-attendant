5d1e32427aff2cf98724109d21d02c8ddd55801c
Jordan Owusu-Ansah <jordanowusu03@gmail.com>
2026-03-12 09:19:37 +0000

    include stationToken in saveDraft payload so draft saves

--- components/washstation/pages/ReportsContent.tsx
+++ components/washstation/pages/ReportsContent.tsx
@@ -192,6 +192,7 @@ function DailyReportPageInner() {
     notes: endOfDayComment || undefined,
     outstandingAmount: autoData?.outstandingAmount ?? 0,
     outstandingOrderCount: autoData?.outstandingOrderCount ?? 0,
+    stationToken: stationToken || undefined,
   });
 
   const handleSaveDraft = async () => {
